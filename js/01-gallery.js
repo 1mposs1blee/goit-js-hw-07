aa8efdf7ab242512c507bd066b7ce94052720ab8
Yaroslav337 <pelerovteatr@gmail.com>
2023-05-28 00:59:38 +0300

    fix 2.0

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,19 +2,21 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryList = document.querySelector(".gallery");
-let basicLightboxObj = null;
+const galleryElemsMarkup = createGalleryElemsMarkup(galleryItems);
+
+galleryList.addEventListener("click", openBigPhoto);
 
 function createGalleryElemsMarkup(arrayItems) {
   return arrayItems
     .map(
-      (el) =>
+      ({ original, preview, description }) =>
         `<li class="gallery__item">
-    <a class="gallery__link" href="${el.original}">
+    <a class="gallery__link" href="${original}">
     <img
-      loading="lazy"
-      class="gallery__image lazyload"
-      data-src="${el.original}"
-      alt="${el.description}"
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
     </a>
     </li>`
@@ -22,71 +24,24 @@ function createGalleryElemsMarkup(arrayItems) {
     .join("");
 }
 
-function addMarkupToList(markup) {
-  galleryList.innerHTML = markup;
-}
+galleryList.innerHTML = galleryElemsMarkup;
 
-function closeModalOnKeyDownEsc(event) {
-  if (event.code !== "Escape") {
-    return;
-  }
-  basicLightboxObj.close();
-}
-
-function createBasicLightboxObj(largeImg) {
-  basicLightboxObj = basicLightbox.create(`<img src=${largeImg}>`, {
-    onShow: () => {
-      document.addEventListener("keydown", closeModalOnKeyDownEsc);
-    },
-    onClose: () => {
-      document.removeEventListener("keydown", closeModalOnKeyDownEsc);
-    },
-  });
-}
-
-function onGalleryListElClick(event) {
+function openBigPhoto(event) {
   event.preventDefault();
+
   if (event.target.nodeName !== "IMG") {
     return;
   }
 
-  const largeImgOfEl = event.target.dataset.src;
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800">`
+  );
 
-  createBasicLightboxObj(largeImgOfEl);
-  basicLightboxObj.show();
-}
+  instance.show();
 
-function createScriptEl(script) {
-  const scriptEl = document.createElement("script");
-
-  scriptEl.src =
-    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
-  scriptEl.integrity =
-    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
-  scriptEl.crossOrigin = "anonymous";
-  scriptEl.referrerPolicy = "no-referrer";
-
-  return scriptEl;
-}
-
-function addSrcToImages(imagesArray) {
-  imagesArray.forEach((image) => {
-    image.src = image.dataset.src;
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
   });
-}
-
-const galleryElemsMarkup = createGalleryElemsMarkup(galleryItems);
-
-addMarkupToList(galleryElemsMarkup);
-
-galleryList.addEventListener("click", onGalleryListElClick);
-
-if ("loading" in HTMLImageElement.prototype) {
-  const allImages = document.querySelectorAll('img[loading="lazy"]');
-
-  addSrcToImages(allImages);
-} else {
-  const script = createScriptEl();
-
-  document.body.append(script);
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,57 +2,24 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryList = document.querySelector(".gallery");
+const galleryElemsMarkup = createGallery(galleryItems);
 
-function createGalleryElemsMarkup(arrayItems) {
+function createGallery(arrayItems) {
   return arrayItems
     .map(
-      (el) =>
+      ({ original, preview, description }) =>
         `<li class="gallery__item">
-   <a class="gallery__link" href="${el.original}">
-      <img loading="lazy" class="gallery__image lazyload" data-src="${el.preview}" alt="${el.description}" />
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${preview}" alt="${description}" />
    </a>
 </li>`
     )
     .join("");
 }
 
-function addMarkupToList(markup) {
-  galleryList.innerHTML = markup;
-}
+galleryList.innerHTML = galleryElemsMarkup;
 
-function createScriptEl(script) {
-  const scriptEl = document.createElement("script");
-
-  scriptEl.src =
-    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
-  scriptEl.integrity =
-    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
-  scriptEl.crossOrigin = "anonymous";
-  scriptEl.referrerPolicy = "no-referrer";
-
-  return scriptEl;
-}
-
-function addSrcToImages(imagesArray) {
-  imagesArray.forEach((image) => {
-    image.src = image.dataset.src;
-  });
-}
-
-const galleryElemsMarkup = createGalleryElemsMarkup(galleryItems);
-addMarkupToList(galleryElemsMarkup);
-
-const modalOfLightbox = new SimpleLightbox(".gallery a", {
+new SimpleLightbox(".gallery a", {
   captionDelay: 250,
   captionsData: "alt",
 });
-
-if ("loading" in HTMLImageElement.prototype) {
-  const allImages = document.querySelectorAll('img[loading="lazy"]');
-
-  addSrcToImages(allImages);
-} else {
-  const script = createScriptEl();
-
-  document.body.append(script);
-}
